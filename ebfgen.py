6b50cd5e0e839e443bb95b4f1fe2e87186ce0d98
Dan Purgert <dan@djph.net>
2020-08-14 11:21:41 -0400

    Add License

--- ebfgen.py
+++ ebfgen.py
@@ -1,18 +1,31 @@
 #!/usr/bin/python3
 ########################################################################
 #
-#
 #                       script: ebfgen
 #                           by: Dan Purgert
 #                    copyright: 2020
 #                      version: 0.5
-#                         date: Wed, 12 Aug 2020 13:27:00 -0400
+#                         date: Fri, 14 Aug 2020 11:20:56 -0400
 #                      purpose: Generates a batch file for upload to
 #                             : the FCC EBF system.
 #
 #                      license: GPL v2 (only)
 #                   repository: https://github.com/dpurgert
 #
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+#  02110-1301 USA.
 #
 ########################################################################
 import tkinter as tk
